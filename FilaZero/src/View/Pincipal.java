9b5e1adb7068d3eea5f60fe8361a83ba6b0ad1ae
PatrickSLuz <patricksantos20101@gmail.com>
2019-04-04 10:17:46 -0300

    Menu Cliente no Principal

--- FilaZero/src/View/Pincipal.java
+++ FilaZero/src/View/Pincipal.java
@@ -5,15 +5,18 @@ import java.util.Scanner;
 public class Pincipal {
 	static Scanner ler = new Scanner(System.in);
 	public static void main(String[] args) {
-		
+
+		menu();
 
 	}
 	
 	public static void menu() {
-		System.out.println("**MENU**");
-		System.out.println("1 -- CLIENTE: ");
-		System.out.println("2 -- ADMINISTRADOR: ");
-		System.out.println("3 -- RESTAURANTE: ");
+		System.out.println("=== MENU ===");
+		System.out.println("1 - Cliente.");
+		System.out.println("2 - Administrador.");
+		System.out.println("3 - Restaurante.");
+		System.out.println("0 - Sair.");
+		System.out.print("Opção: ");
 		int op = ler.nextInt();
 		
 		switch (op) {
@@ -24,11 +27,32 @@ public class Pincipal {
 			adm();
 			break;
 		case 3:
-			restaurente();
+			cozinha();
+			break;
+		case 0:
+			op = 0;
 			break;
 		default:
+			System.out.println("\nOpção Inválida!");
 			break;
 		}
+	}
+	
+	public static void menuCliente() {
+		System.out.println("\n=== MENU CLIENTE ===");
+		System.out.println("1 - Cadastrar cliente.");
+		System.out.println("2 - Fazer Login.");
+		System.out.println("0 - Voltar.");
+		System.out.print("Opção: ");
+		int opcao = ler.nextInt();
+	}
+	
+	public static void adm() {
+		
+	}
+	
+	public static void cozinha() {
+		
 	}
 	
 	public static void cadcliente() {
