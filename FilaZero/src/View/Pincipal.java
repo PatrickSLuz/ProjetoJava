62d77725853018bff4c29c543426053577147a3e
Mauriciokreling <mauriciokreling10@gmail.com>
2019-04-04 09:58:31 -0300

    Atualização

--- FilaZero/src/View/Pincipal.java
+++ FilaZero/src/View/Pincipal.java
@@ -15,12 +15,36 @@ public class Pincipal {
 		System.out.println("2 -- ADMINISTRADOR: ");
 		System.out.println("3 -- RESTAURANTE: ");
 		int op = ler.nextInt();
-			
+		
+		switch (op) {
+		case 1:
+			menuCliente();
+			break;
+		case 2: 
+			adm();
+			break;
+		case 3:
+			restaurente();
+			break;
+		default:
+			break;
+		}
 	}
 	
 	public static void cadcliente() {
 		System.out.println("INFORME SEU NOME: ");
-		System.out.println("INFORME SEU CFP");
+		String nome = ler.next();
+		
+		System.out.println("INFORME SEU RG: ");
+		int rg = ler.nextInt();
+		
+		System.out.println("INFORME SEU CPF: ");
+		int cpf = ler.nextInt();
+		
+		System.out.println("INFORME SEU TELEFONE: ");
+		String telefone = ler.next();
+		System.out.println("INFORME SEU ENDEREÇO: ");
+		
 	}
 	
 }
