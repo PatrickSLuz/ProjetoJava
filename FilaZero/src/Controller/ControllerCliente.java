409f25ceb4dba5f23a851a5469d0025b5cf27708
Mauriciokreling <mauriciokreling10@gmail.com>
2019-04-04 11:34:09 -0300

    Adcionado cadastro cliente e login

--- FilaZero/src/Controller/ControllerCliente.java
+++ FilaZero/src/Controller/ControllerCliente.java
@@ -9,4 +9,19 @@ public class ControllerCliente {
 
 	List<Cliente> listCliente = new ArrayList<Cliente>();
 	
+	public void cadCliente(Cliente cliente) {
+		listCliente.add(cliente);
+	}
+	
+	public Cliente autenticarCliente(String login, String senha) {
+		Cliente logado=null;
+		for(int i =0; i < listCliente.size() ; i++) {
+			if(listCliente.get(i).getLogin().equals(login) && listCliente.get(i).getSenha().equals(senha)) {
+				logado = listCliente.get(i);
+				break;
+			}
+		}
+		return logado;
+	}
+	
 }
